f2fb4acbe07af09e3f62d126d0a0629747b170e5
mexinux <mexinux@pm.me>
2022-02-02 22:04:20 -0600

    Calcular el area de un terreno dadas su frente y fondo

---
+++ catastro.py
@@ -0,0 +1,9 @@
+'''
+La direccion de catastro desea calcular el area de un terreno dadas las medidas de frente y fondo
+'''
+base = float(input("Ingresa la medida del frente"))
+altura = float(input("Ingresa la medida del fondo"))
+
+area = base * altura
+
+print("El area del terreno es:",area)
