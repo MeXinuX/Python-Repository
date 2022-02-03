dc838b39da8c4abec9288137fc264036966b7af3
mexinux <mexinux@pm.me>
2022-02-02 21:50:37 -0600

    Calcular el area de un cuadrado ingresando uno de sus lados

---
+++ areaCuadrado.py
@@ -0,0 +1,11 @@
+'''
+Calcular el area de un cuadrado ,dada la longitud de uno de sus lados
+'''
+
+#la formula del area del cuadraro es lxl o l²
+
+lado = int(input("Ingresa la medida de un lado del cuadrado"))
+
+area = lado * lado
+
+print("El area del cuadrado es:",area)
